0162de74bbeec079ca88782af540a77d249ba9c1
Antonin Delpeuch <antonin@delpeuch.eu>
2020-01-25 15:25:05 +0100

    Remove STRICT_TRANS_TABLES

--- editgroups/settings/secret_travis.py
+++ editgroups/settings/secret_travis.py
@@ -13,7 +13,6 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': 'localhost',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         }
     }
